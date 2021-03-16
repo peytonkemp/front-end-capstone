0f5e73d6b3da65d1d3ec36f37daa36328d869e72
peytonkemp <peyton.kemp@gmail.com>
2021-03-16 15:38:08 -0500

    setup api component and login

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import TeeBox from './TeeBox';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<TeeBox />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from "react-router-dom"
+import { TeeBox } from './components/TeeBox.js';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <TeeBox />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
