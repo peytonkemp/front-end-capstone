84e8798cfe18f63a2803aee2e7a8d52292899c0e
peytonkemp <peyton.kemp@gmail.com>
2021-03-17 14:06:40 -0500

    built out course provider and started on RoundList

---
+++ src/components/courses/CourseProvider.js
@@ -0,0 +1,34 @@
+import { useState, createContext } from "react";
+
+export const CourseContext = createContext()
+
+export const CourseProvider = (props) => {
+    const [ courses, setCourses ] = useState([])
+
+    const getCourses = () => {
+        return fetch("http://localhost:8088/courses")
+        .then(response => response.json())
+        .then(setRounds)
+    }
+
+    const addCourse = courseObj => {
+        return fetch("http://localhost:8088/courses", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(courseObj)
+        })
+        .then(getCourses)
+    }
+
+
+    return (
+        <CourseContext.Provider value={{
+            courses, getCourses, addCourse
+        }}>
+            {props.children}
+        </CourseContext.Provider>
+     )
+
+}
