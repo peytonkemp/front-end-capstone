34b80640deb81fa37c1566299fd99cc428b964e8
peytonkemp <peyton.kemp@gmail.com>
2021-03-23 12:49:47 -0500

    added to event.preventDefault()

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from "react-router-dom"
 import { TeeBox } from './components/TeeBox.js';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 
 ReactDOM.render(
